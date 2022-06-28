2fd7754ecba35601652697a94c05e48251b55e6e
Juan Carlos Ortiz Drada <jdrada.dev@gmail.com>
2022-06-28 21:10:32 +0200

    feat: add infinite scroll in search page

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,9 +1,74 @@
-import React from "react";
+import { Container } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import GifCard from "../components/GifCard";
+import { GiphyController } from "../services/giphy.service";
 
 const Search = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [gifs, setGifs] = useState<any>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(true);
+  const [counter, setCounter] = useState<number>(1);
+  const [blockRequest, setBlockRequest] = useState<boolean>(false);
+
+  const fetchMoreData = async () => {
+    if (!blockRequest) {
+      const gifsResponse = GiphyController.searchGifs(
+        searchInput,
+        10,
+        counter * 10
+      );
+      const newGifs = await gifsResponse;
+      setGifs((oldGifs: any) => [...oldGifs, ...newGifs]);
+      console.log(newGifs);
+      setIsFetching(false);
+      setCounter((oldCounter) => oldCounter + 1);
+      setBlockRequest(true);
+    }
+  };
+
+  const searchHandler = (event: any) => {
+    setSearchInput(event.target.value);
+  };
+
+  const searchGifs = async () => {
+    setGifs([]);
+    await fetchMoreData();
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setBlockRequest(false);
+    }, 3000);
+  }, [gifs]);
+
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Buscar</h2>
+      <div>
+        <div>
+          <input onChange={searchHandler} type="text" />
+          <button onClick={searchGifs}>Buscar</button>
+        </div>
+        <div>
+          <Container>
+            <InfiniteScroll
+              dataLength={gifs.length}
+              next={fetchMoreData}
+              hasMore={true}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              {gifs.map((gif: any) => (
+                <GifCard key={gif.id} gif={gif}></GifCard>
+              ))}
+            </InfiniteScroll>
+          </Container>
+        </div>
+      </div>
     </main>
   );
 };
--- src/services/giphy.service.ts
+++ src/services/giphy.service.ts
@@ -12,4 +12,17 @@ export const GiphyController = {
 
     return results.data.data;
   },
+
+  async searchGifs(query: string, limit = 10, offset = 0) {
+    const results = await axios("https://api.giphy.com/v1/gifs/search", {
+      params: {
+        api_key: "5mjiA0lqt5ZKgi270auAuqv5dvOMjrD9",
+        q: query,
+        limit,
+        offset,
+      },
+    });
+
+    return results.data.data;
+  },
 };
